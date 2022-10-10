4c6d37c74a886d9918eb3382b378af4a290c403c
Benji Lampel <benjamin@astronomer.io>
2022-10-10 12:24:51 -0400

    Update lineage_demo.py

--- dags/lineage_demo.py
+++ dags/lineage_demo.py
@@ -5,35 +5,14 @@ from airflow.utils.dates import days_ago
 from airflow.decorators import dag
 from airflow.utils.task_group import TaskGroup
 
-default_args = {
-    'owner': 'registry',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email_on_failure': False,
-    'email_on_retry': False
-}
 
-# When using the DAG decorator, the "dag" argument doesn't need to be specified for each task.
-# The "dag_id" value defaults to the name of the function it is decorating if not explicitly set.
-# In this example, the "dag_id" value would be "example_dag_advanced".
 @dag(
-    # This DAG is set to run for the first time on June 11, 2021. Best practice is to use a static start_date.
-    # Subsequent DAG runs are instantiated based on scheduler_interval below.
     start_date=days_ago(1),
-    # This defines how many instantiations of this DAG (DAG Runs) can execute concurrently. In this case,
-    # we're only allowing 1 DAG run at any given time, as opposed to allowing multiple overlapping DAG runs.
     max_active_runs=1,
-    # This defines how often your DAG will run, or the schedule by which DAG runs are created. It can be
-    # defined as a cron expression or custom timetable. This DAG will run daily.
     schedule_interval=None,
-    # Default settings applied to all tasks within the DAG; can be overwritten at the task level.
-    default_args=default_args,
-    default_view="graph", # This defines the default view for this DAG in the Airflow UI
-    # When catchup=False, your DAG will only run for the latest schedule interval. In this case, this means
-    # that tasks will not be run between June 11, 2021 and 1 day ago. When turned on, this DAG's first run
-    # will be for today, per the @daily schedule interval
+    default_view="graph",
     catchup=False,
-    tags=["demo"], # If set, this tag is shown in the DAG view of the Airflow UI
+    tags=["demo"],
 )
 def lineage_demo():
 
